6f5dfc68f57f21b1bbd53aba3331369359577a51
Shoham Brojobasi <shohambrojobasi@gmail.com>
2024-12-05 19:55:23 +0530

    Update Pricing.jsx
    Deployment update

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,5 +1,5 @@
 import "./Pricing.scss";
-import { pricingInfo } from "../../data";
+import { pricingInfo } from "../../Data";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
